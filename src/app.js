1ef9aedaad767da3856054d3138fb2751a42ae03
arbin Thapa <arbinbabuthapamagar2002@gmail.com>
2026-02-06 23:38:29 +0545

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmat from "helmet";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmat({
+    crossOriginEmbedderPolicy: false,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the BudiAmaKoPasal API" });
+});
+
+export default app;
