d65714700517df37753ffbc0df395db18c697b67
arbin Thapa <arbinbabuthapamagar2002@gmail.com>
2026-02-06 23:49:55 +0545

    first commit

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the BudiAmaKoPasal API" });
+  res.json({ message: "Welcome to the VintunaStore" });
 });
 
 export default app;
